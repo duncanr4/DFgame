da4a462e39f657e8fdb1ed626592d8146727101f
nativenewengland <maxeconte@gmail.com>
2026-02-12 05:03:58 -0800

    Refactor overworld generation into domain modules

---
+++ scripts/world_generation/biome_defs.gd
@@ -0,0 +1,13 @@
+class_name OverworldBiomeDefs
+extends RefCounted
+
+const WATER := "water"
+const MOUNTAIN := "mountain"
+const HILLS := "hills"
+const MARSH := "marsh"
+const TUNDRA := "tundra"
+const DESERT := "desert"
+const BADLANDS := "badlands"
+const FOREST := "forest"
+const JUNGLE := "jungle"
+const GRASSLAND := "grassland"
